d2e581bd8583fb2f7f0dad15885fff2987e63c01
Francis Normand <francis.normand@canada.ca>
2019-04-30 14:08:03 -0400

    Created a new component called 'Dashboard' + Added wording from text_resources + Connected it with redux login states

---
+++ frontend/src/Dashboard.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import LOCALIZE from "./text_resources";
+import ContentContainer from "./components/commons/ContentContainer";
+import { Helmet } from "react-helmet";
+
+const styles = {
+  title: {
+    color: "black"
+  }
+};
+
+class Dashboard extends Component {
+  static propTypes = {
+    // Props from Redux
+    loggedIn: PropTypes.bool
+  };
+
+  render() {
+    return (
+      <ContentContainer>
+        <Helmet>
+          <title>{LOCALIZE.titles.dashboard}</title>
+        </Helmet>
+        <h2 className="green-divider" style={styles.title}>
+          {LOCALIZE.dashboard.title}
+        </h2>
+        {!this.props.loggedIn && <p>{LOCALIZE.dashboard.descriptionIfLoggedOut}</p>}
+        {this.props.loggedIn && <p>{LOCALIZE.dashboard.descriptionIfLoggedIn}</p>}
+      </ContentContainer>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    loggedIn: state.login.loggedIn
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(Dashboard);
