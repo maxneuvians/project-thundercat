f52f05e6ef75ebffad598a9751af65b47006867d
Michael Cherry <zorackiii@gmail.com>
2019-05-01 11:11:30 -0400

    Adding first diabled component to Instructions

--- frontend/src/components/eMIB/TestInstructions.jsx
+++ frontend/src/components/eMIB/TestInstructions.jsx
@@ -2,10 +2,40 @@ import React, { Component } from "react";
 import "../../css/lib/aurora.min.css";
 import LOCALIZE from "../../text_resources";
 import "../../css/cat-theme.css";
+import Email from "./Email";
+import ActionViewEmail from "./ActionViewEmail";
+import ActionViewTask from "./ActionViewTask";
 import { styleConstants } from "./styleConstants.js";
+import { ACTION_TYPE, EMAIL_TYPE } from "./constants";
 
 class TestInstructions extends Component {
   render() {
+    const exampleEmail = {
+      id: 0,
+      to: LOCALIZE.emibTest.howToPage.testExamples.exampleEmail.to,
+      from: LOCALIZE.emibTest.howToPage.testExamples.exampleEmail.from,
+      subject: LOCALIZE.emibTest.howToPage.testExamples.exampleEmail.subject,
+      date: LOCALIZE.emibTest.howToPage.testExamples.exampleEmail.date,
+      body: LOCALIZE.emibTest.howToPage.testExamples.exampleEmail.body
+    };
+
+    const exampleEmailResponse = {
+      actionType: ACTION_TYPE.email,
+      emailType: EMAIL_TYPE.reply,
+      emailTo: [8], // Geneviève Bédard in the address book
+      emailCc: [],
+      emailBody: LOCALIZE.emibTest.howToPage.testExamples.exampleEmailResponse.emailBody,
+      reasonsForAction:
+        LOCALIZE.emibTest.howToPage.testExamples.exampleEmailResponse.reasonsForAction
+    };
+
+    const exampleTaskResponse = {
+      actionType: ACTION_TYPE.task,
+      task: LOCALIZE.emibTest.howToPage.testExamples.exampleTaskResponse.task,
+      reasonsForAction:
+        LOCALIZE.emibTest.howToPage.testExamples.exampleTaskResponse.reasonsForAction
+    };
+
     return (
       <div>
         <div>
@@ -22,6 +52,12 @@ class TestInstructions extends Component {
             <p style={styleConstants.instuctions.p}>
               {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.description}
             </p>
+            <h4 style={styleConstants.instuctions.h4}>
+              ===TODO: MOVE=== Example of an email you have recieved:
+            </h4>
+            <div style={styleConstants.instuctions.disabledExampleComponentNoPadding}>
+              <Email email={exampleEmail} disabled={true} />
+            </div>
             <h4 style={styleConstants.instuctions.h4}>
               {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part1.title}
             </h4>
