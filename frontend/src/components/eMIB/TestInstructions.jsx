2be20803f31fe80c3c8c69ca10177b72dd5624ff
Michael Cherry <zorackiii@gmail.com>
2019-05-01 11:26:19 -0400

    Shifting parts for Section1 up by 1

--- frontend/src/components/eMIB/TestInstructions.jsx
+++ frontend/src/components/eMIB/TestInstructions.jsx
@@ -60,19 +60,13 @@ class TestInstructions extends Component {
               <Email email={exampleEmail} disabled={true} />
             </div>
             <h4 style={styleConstants.instuctions.h4}>
-              {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part1.title}
-            </h4>
-            <p style={styleConstants.instuctions.p}>
-              {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part1.para1}
-            </p>
-            <p style={styleConstants.instuctions.p}>
-              {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part1.para2}
-            </p>
-            <h4 style={styleConstants.instuctions.h4}>
               {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part2.title}
             </h4>
             <p style={styleConstants.instuctions.p}>
               {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part2.para1}
+            </p>
+            <p style={styleConstants.instuctions.p}>
+              {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part2.para2}
             </p>
             <h4 style={styleConstants.instuctions.h4}>
               {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part3.title}
@@ -80,16 +74,22 @@ class TestInstructions extends Component {
             <p style={styleConstants.instuctions.p}>
               {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part3.para1}
             </p>
+            <h4 style={styleConstants.instuctions.h4}>
+              {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part4.title}
+            </h4>
+            <p style={styleConstants.instuctions.p}>
+              {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part4.para1}
+            </p>
             <ol>
-              <li>{LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part3.bullet1}</li>
-              <li>{LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part3.bullet2}</li>
-              <li>{LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part3.bullet3}</li>
+              <li>{LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part4.bullet1}</li>
+              <li>{LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part4.bullet2}</li>
+              <li>{LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part4.bullet3}</li>
             </ol>
             <p style={styleConstants.instuctions.p}>
-              {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part3.para2}
+              {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part4.para2}
             </p>
             <p style={styleConstants.instuctions.p}>
-              {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part3.para3}
+              {LOCALIZE.emibTest.howToPage.testInstructions.step1Section.part4.para3}
             </p>
           </div>
           <div>
