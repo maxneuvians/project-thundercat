3e9ad8ade7ba9c471bf404ff29b140839e8f4eaa
Caley Brock <caleybrock7@gmail.com>
2019-05-03 08:45:06 -0400

    Replace side nav with bootstrap side nav

--- frontend/src/components/eMIB/Background.jsx
+++ frontend/src/components/eMIB/Background.jsx
@@ -4,7 +4,24 @@ import TeamInformation from "./TeamInformation";
 import BackgroundInformation from "./BackgroundInformation";
 import OrganizationalInformation from "./OrganizationalInformation";
 import OrganizationalStructure from "./OrganizationalStructure";
-import SideNavigation from "../commons/SideNavigation";
+import { HEADER_HEIGHT, FOOTER_HEIGHT } from "../eMIB/constants";
+import { Tab, Row, Col, Nav } from "react-bootstrap";
+
+const BODY_HEIGHT = `calc(100vh - ${HEADER_HEIGHT + FOOTER_HEIGHT}px)`;
+
+const styles = {
+  bodyContent: {
+    overflow: "auto",
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    height: BODY_HEIGHT
+  },
+  nav: {
+    marginTop: 10,
+    marginLeft: 10
+  }
+};
 
 export const getInstructionContent = () => {
   return [
@@ -35,11 +52,42 @@ class Background extends Component {
   render() {
     const SPECS = getInstructionContent();
     return (
-      <SideNavigation
-        navSpecs={SPECS}
-        currentNode={0}
-        menuName={LOCALIZE.ariaLabel.backgroundMenu}
-      />
+      <Tab.Container id="left-tabs-instructions" defaultActiveKey="first">
+        <Row>
+          <Col sm={3}>
+            <Nav variant="pills" className="flex-column" style={styles.nav}>
+              <Nav.Item>
+                <Nav.Link eventKey="first">{SPECS[0].text}</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">{SPECS[1].text}</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="third">{SPECS[2].text}</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="fourth">{SPECS[3].text}</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={9}>
+            <Tab.Content>
+              <Tab.Pane eventKey="first">
+                <div style={styles.bodyContent}>{SPECS[0].body}</div>
+              </Tab.Pane>
+              <Tab.Pane eventKey="second">
+                <div style={styles.bodyContent}>{SPECS[1].body}</div>
+              </Tab.Pane>
+              <Tab.Pane eventKey="third">
+                <div style={styles.bodyContent}>{SPECS[2].body}</div>
+              </Tab.Pane>
+              <Tab.Pane eventKey="fourth">
+                <div style={styles.bodyContent}>{SPECS[3].body}</div>
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     );
   }
 }
--- frontend/src/components/eMIB/InTestInstructions.jsx
+++ frontend/src/components/eMIB/InTestInstructions.jsx
@@ -1,17 +1,74 @@
 import React, { Component } from "react";
 import LOCALIZE from "../../text_resources";
-import SideNavigation from "../commons/SideNavigation";
-import { getInstructionContent } from "./Emib";
+import { Tab, Row, Col, Nav } from "react-bootstrap";
+import TipsOnTest from "./TipsOnTest";
+import TestInstructions from "./TestInstructions";
+import Evaluation from "./Evaluation";
+import { HEADER_HEIGHT, FOOTER_HEIGHT } from "../eMIB/constants";
+
+const BODY_HEIGHT = `calc(100vh - ${HEADER_HEIGHT + FOOTER_HEIGHT}px)`;
+
+const styles = {
+  bodyContent: {
+    overflow: "auto",
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    height: BODY_HEIGHT
+  },
+  nav: {
+    marginTop: 10,
+    marginLeft: 10
+  }
+};
+
+//Returns array where each item indicates specifications related to How To Page including the title and the body
+const getInstructionContent = () => {
+  return [
+    {
+      id: 0,
+      text: LOCALIZE.emibTest.howToPage.testInstructions.title,
+      body: <TestInstructions />
+    },
+    { id: 1, text: LOCALIZE.emibTest.howToPage.tipsOnTest.title, body: <TipsOnTest /> },
+    { id: 2, text: LOCALIZE.emibTest.howToPage.evaluation.title, body: <Evaluation /> }
+  ];
+};
 
 class InTestInstructions extends Component {
   render() {
     const SPECS = getInstructionContent();
     return (
-      <SideNavigation
-        navSpecs={SPECS}
-        currentNode={0}
-        menuName={LOCALIZE.ariaLabel.instructionsMenu}
-      />
+      <Tab.Container id="left-tabs-instructions" defaultActiveKey="first">
+        <Row>
+          <Col sm={3}>
+            <Nav variant="pills" className="flex-column" style={styles.nav}>
+              <Nav.Item>
+                <Nav.Link eventKey="first">{SPECS[0].text}</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">{SPECS[1].text}</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="third">{SPECS[2].text}</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={9}>
+            <Tab.Content>
+              <Tab.Pane eventKey="first">
+                <div style={styles.bodyContent}>{SPECS[0].body}</div>
+              </Tab.Pane>
+              <Tab.Pane eventKey="second">
+                <div style={styles.bodyContent}>{SPECS[1].body}</div>
+              </Tab.Pane>
+              <Tab.Pane eventKey="third">
+                <div style={styles.bodyContent}>{SPECS[2].body}</div>
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     );
   }
 }
