73c23a7646cdd96c894e0db3b4e3743334f443e6
Francis Normand <francis.normand@canada.ca>
2019-05-06 11:09:47 -0400

    Replaced our backend with the login example backend - WIP

--- backend/custom_user/admin.py
+++ backend/custom_user/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from backend.models.user_model import User
+from .models import User
 from django.contrib.auth.admin import UserAdmin
 
 admin.site.register(User, UserAdmin)
