c12d23e29345fac7ab372474d5218deb46e65e3f
Francis Normand <francis.normand@canada.ca>
2019-05-07 14:58:25 -0400

    Updated swagger title

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -8,7 +8,7 @@ from views import views, database_check_view, user_view
 
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title="Pastebin API")
+schema_view = get_swagger_view(title="ThunderCAT APIs")
 
 router = routers.DefaultRouter()
 router.register(r"database_check", database_check_view.DatabaseViewSet)
