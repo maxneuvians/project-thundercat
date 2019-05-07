93a33fa770e4813af302d06fa6ed7a12f13176d9
Michael Cherry <zorackiii@gmail.com>
2019-05-07 14:42:16 -0400

    Moved tree.js from css/lib to helpers/lib

--- frontend/src/components/commons/TreeNode.jsx
+++ frontend/src/components/commons/TreeNode.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import "../../css/lib/tree";
+import "../../helpers/lib/tree";
 import "../../css/lib/tree.css";
 
 // The structure of a node in a treeview
