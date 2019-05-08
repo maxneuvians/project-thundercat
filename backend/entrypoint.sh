3ab2523727d9fd9dda06a131bbabf36572af3bb4
Francis Normand <francis.normand@canada.ca>
2019-05-08 08:01:08 -0400

    Updated backend entrypoint.sh file

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,5 @@
 #!bin/bash
-python manage.py makemigrations
+python manage.py makemigrations custom_models
+python manage.py migrate custom_models
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
