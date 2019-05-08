81004a28d3407428973c5b6ed03be25ef0e5d89d
Francis Normand <francis.normand@canada.ca>
2019-05-08 11:19:46 -0400

    Removed unused 'setLoginState' redux action from the LoginRedux file

--- frontend/src/modules/LoginRedux.js
+++ frontend/src/modules/LoginRedux.js
@@ -1,5 +1,4 @@
 // Action Types
-export const SET_LOGIN_STATE = "localize/SET_LOGIN_STATE";
 // URL
 export const BACKENDURL = "http://localhost:8000";
 // AUTH ACTIONS
@@ -13,8 +12,6 @@ export const CHANGE_PASSWORD_SUCCESS = "CHANGE_PASSWORD_SUCCESS";
 export const CHANGE_PASSWORD_FAILURE = "CHANGE_PASSWORD_FAILURE";
 
 // Action Creators
-const setLoginState = loggedIn => ({ type: SET_LOGIN_STATE, loggedIn });
-
 let url = process.env.REACT_APP_DEV_URL || BACKENDURL;
 
 // function authenticateAction(userData, dispatch, location, push) {
@@ -82,11 +79,6 @@ const registration_message = "You have been registered successfully.";
 // Reducer
 const login = (state = initialState, action) => {
   switch (action.type) {
-    case SET_LOGIN_STATE:
-      return {
-        ...state,
-        loggedIn: action.loggedIn
-      };
     case AUTHENTICATED:
       //return { authenticated: true, registration_message: "" };
       return {
@@ -105,7 +97,6 @@ const login = (state = initialState, action) => {
 
 export default login;
 export {
-  setLoginState,
   initialState,
   registerAction,
   loginAction,
