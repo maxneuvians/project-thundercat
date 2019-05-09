91d0d09fe6c6cb4943dc85fe8ba361079a8b01bd
Francis Normand <francis.normand@canada.ca>
2019-05-09 10:21:34 -0400

    Removed user serializer, since there is no user view anymore

--- backend/serializers/user_serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from custom_models.models import User
-
-# Serializers define the API representation
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ("email", "username", "password")
