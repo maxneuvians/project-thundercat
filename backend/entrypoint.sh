3a77bb2f6ac80a9566f5e443cedaef1f5031bb75
Francis Normand <francis.normand@canada.ca>
2019-05-09 15:21:19 -0400

    Added configs for the backend container + Updated container name of postgres container

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,9 @@
 #!bin/bash
-python manage.py makemigrations
+
+# all our models should be under custom_models
+# checking if there are any new migrations in custom_models
+python manage.py makemigrations custom_models
+# all initial and new migrations will be migrated
 python manage.py migrate
-python manage.py runserver
+# running web server on port 8000
+python manage.py runserver 0.0.0.0:8000
