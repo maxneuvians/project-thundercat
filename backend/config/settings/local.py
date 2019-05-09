50cc354f59cf2fc974ce933b21e02a3b06cdf67e
Francis Normand <francis.normand@canada.ca>
2019-05-09 14:02:52 -0400

    Completed the postgres setup, so we can connect to the DB with VS Code + Updated entrypoint (backend)

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -9,21 +9,13 @@ INTERNAL_IPS = ["127.0.0.1"]
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# TODO(fnormand): make the connection working with postgres
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "HOST": "db",
-#         "PORT": 5432,
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 
